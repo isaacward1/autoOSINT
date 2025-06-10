0f5d64c4877e16b576c6a89dd47b3793bb90a1bc
Isaac Ward <165718547+isaacward1@users.noreply.github.com>
2025-06-10 00:53:42 -0400

    Update autoOSINT.py

--- autoOSINT.py
+++ autoOSINT.py
@@ -4,37 +4,42 @@ import webbrowser
 import re
 import os
 import sys
+import time
 
 IP_links = [
-'https://cyberfl.splunkcloud.com/en-US/app/TA-recordedfuture/rfes_enrich_ip?form.name={ioc}',    # requires login
-'https://www.virustotal.com/gui/ip-address/{ioc}',
-'https://www.abuseipdb.com/check/{ioc}',
-'https://centralops.net/co/DomainDossier.aspx?addr={ioc}&dom_dns=true&dom_whois=true&net_whois=true',
-'https://urlscan.io/ip/{ioc}',
-'https://www.securefeed.com/Content/WebLookup?host={ioc}',
-'https://viz.greynoise.io/ip/{ioc}',
-'https://threatfox.abuse.ch/browse.php?search=ioc%3A{ioc}',
-'https://otx.alienvault.com/indicator/ip/{ioc}',
-'https://www.hybrid-analysis.com/search?query={ioc}',     # requires login
-'https://www.joesandbox.com/analysis/search?q={ioc}',
-#'https://tria.ge/s?q={ioc}',
-r'https://www.google.com/search?q="{ioc}"+site:any.run',
-#r'https://www.google.com/search?q="{ioc}"'
+    'https://cyberfl.splunkcloud.com/en-US/app/TA-recordedfuture/rfes_enrich_ip?form.name={ioc}',    # requires login
+    'https://www.virustotal.com/gui/ip-address/{ioc}',
+    'https://www.abuseipdb.com/check/{ioc}',
+    'https://centralops.net/co/DomainDossier.aspx?addr={ioc}&dom_dns=true&dom_whois=true&net_whois=true',
+    'https://urlscan.io/ip/{ioc}',
+    'https://www.securefeed.com/Content/WebLookup?host={ioc}',
+    #'https://viz.greynoise.io/ip/{ioc}',
+    'https://threatfox.abuse.ch/browse.php?search=ioc%3A{ioc}',
+    'https://otx.alienvault.com/indicator/ip/{ioc}',
+    #'https://threatbook.io/ip/{ioc}',
+    'https://socradar.io/labs/app/ioc-radar/{ioc}',
+    #'https://www.hybrid-analysis.com/search?query={ioc}',     # requires login
+    #'https://www.joesandbox.com/analysis/search?q={ioc}',
+    #'https://tria.ge/s?q={ioc}',
+    r'https://www.google.com/search?q="{ioc}"+site:any.run+OR+site:www.joesandbox.com+OR+site:www.hybrid-analysis.com',
+    #r'https://www.google.com/search?q="{ioc}"'
 ]
 
 domain_links = [
-'https://cyberfl.splunkcloud.com/en-US/app/TA-recordedfuture/rfes_enrich_domain?form.name={ioc}',    # requires login
-'https://www.virustotal.com/gui/domain/{ioc}',
-'https://urlscan.io/domain/{ioc}',
-'https://centralops.net/co/DomainDossier.aspx?addr={ioc}&dom_dns=true&dom_whois=true&net_whois=true',
-'https://www.securefeed.com/Content/WebLookup?host={ioc}',
-'https://threatfox.abuse.ch/browse.php?search=ioc%3A{ioc}',
-'https://otx.alienvault.com/indicator/hostname/{ioc}',
-'https://www.hybrid-analysis.com/search?query={ioc}',     # requires login
-'https://www.joesandbox.com/analysis/search?q={ioc}',
-#'https://tria.ge/s?q={ioc}',
-r'https://www.google.com/search?q="{ioc}"+site:any.run',
-#r'https://www.google.com/search?q="{ioc}"'
+    'https://cyberfl.splunkcloud.com/en-US/app/TA-recordedfuture/rfes_enrich_domain?form.name={ioc}',    # requires login
+    'https://www.virustotal.com/gui/domain/{ioc}',
+    'https://urlscan.io/domain/{ioc}',
+    'https://centralops.net/co/DomainDossier.aspx?addr={ioc}&dom_dns=true&dom_whois=true&net_whois=true',
+    'https://www.securefeed.com/Content/WebLookup?host={ioc}',
+    'https://threatfox.abuse.ch/browse.php?search=ioc%3A{ioc}',
+    'https://otx.alienvault.com/indicator/hostname/{ioc}',
+    #'https://threatbook.io/domain/{ioc}',
+    'https://socradar.io/labs/app/ioc-radar/{ioc}',
+    #'https://www.hybrid-analysis.com/search?query={ioc}',     # requires login
+    #'https://www.joesandbox.com/analysis/search?q={ioc}',
+    #'https://tria.ge/s?q={ioc}',
+    r'https://www.google.com/search?q="{ioc}"+site:any.run+OR+site:www.joesandbox.com+OR+site:www.hybrid-analysis.com',
+    #r'https://www.google.com/search?q="{ioc}"'
 ]
 
 subs = {
@@ -62,12 +67,14 @@ def search_IOC():
             for link in IP_links:
                 new_link = re.sub(r"\{ioc\}", ioc, link)
                 webbrowser.open_new_tab(new_link)
+                time.sleep(0.5)
 
         # if domain
         elif re.match(domain_pattern, ioc) is not None:
             for link in domain_links:
                 new_link = re.sub(r"\{ioc\}", ioc, link)
                 webbrowser.open_new_tab(new_link)
+                time.sleep(0.5)
 
         # if ioc match no pattern
         else:
